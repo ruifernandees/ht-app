36fb5dfcac7fe54646e2cc6e298127aa6fa6801d
ruifernandees <dev.ruifernandes@gmail.com>
2024-03-17 21:46:52 -0300

    feat: create settings hookform

---
+++ src/presentation/screens/settings/data.tsx
@@ -0,0 +1,77 @@
+import * as zod from 'zod'
+
+type TAutoCapitalize = "none" | "sentences" | "words" | "characters" | undefined
+
+export const inputs = [
+  {
+    placeholder: 'Cor do objeto 1',
+    name: 'color1',
+    autoCapitalize: 'none' as TAutoCapitalize,
+  },
+  {
+    placeholder: 'Cor do objeto 2',
+    name: 'color2',
+    autoCapitalize: 'none' as TAutoCapitalize,
+  },
+  {
+    placeholder: 'Cor do objeto 3',
+    name: 'color3',
+    autoCapitalize: 'none' as TAutoCapitalize,
+  },
+  {
+    placeholder: 'Rotação do objeto 1',
+    name: 'rotation1',
+    autoCapitalize: 'none' as TAutoCapitalize,
+  },
+  {
+    placeholder: 'Rotação do objeto 2',
+    name: 'rotation2',
+    autoCapitalize: 'none' as TAutoCapitalize,
+  },
+  {
+    placeholder: 'Rotação do objeto 3',
+    name: 'rotation3',
+    autoCapitalize: 'none' as TAutoCapitalize,
+  },
+]
+
+const COLOR_REQUIRED_MESSAGE = 'Informe a cor em Hexadecimal'
+
+export const FormSchema = zod.object({
+  color1: zod
+    .string({ required_error: COLOR_REQUIRED_MESSAGE })
+    .min(1, COLOR_REQUIRED_MESSAGE),
+  color2: zod
+    .string({ required_error: COLOR_REQUIRED_MESSAGE })
+    .min(1, COLOR_REQUIRED_MESSAGE),
+  color3: zod
+    .string({ required_error: COLOR_REQUIRED_MESSAGE })
+    .min(1, COLOR_REQUIRED_MESSAGE),
+  shape1: zod
+    .string({ required_error: COLOR_REQUIRED_MESSAGE })
+    .min(1, COLOR_REQUIRED_MESSAGE),
+  shape2: zod
+    .string({ required_error: COLOR_REQUIRED_MESSAGE })
+    .min(1, COLOR_REQUIRED_MESSAGE),
+  shape3: zod
+    .string({ required_error: COLOR_REQUIRED_MESSAGE })
+    .min(1, COLOR_REQUIRED_MESSAGE),
+  rotation1: zod
+    .string({ required_error: COLOR_REQUIRED_MESSAGE })
+    .min(1, COLOR_REQUIRED_MESSAGE),
+  rotation2: zod
+    .string({ required_error: COLOR_REQUIRED_MESSAGE })
+    .min(1, COLOR_REQUIRED_MESSAGE),
+  rotation3: zod
+    .string({ required_error: COLOR_REQUIRED_MESSAGE })
+    .min(1, COLOR_REQUIRED_MESSAGE)
+
+});
+
+export const options = [
+  { label: 'Cone', value: 'cone' },
+  { label: 'Tetraedro', value: 'tetrahedron' },
+  { label: 'Dodecaedro', value: 'dodecahedron' },
+  { label: 'Cubo', value: 'cube' },
+  { label: 'Nó', value: 'torusKnot' }
+]
--- src/presentation/screens/settings/index.tsx
+++ src/presentation/screens/settings/index.tsx
@@ -1,17 +1,34 @@
 import React, { useState } from 'react';
-import { ButtonText, Container, Icon, OptionButton,  } from './styles';
+import { ButtonText, Container, ErrorText, Icon, Input, InputContainer, OptionButton,  } from './styles';
 import { Header } from '@/presentation/components/Header';
 import { useAuthenticationStore } from '@/presentation/stores/authentication';
-import { EAppStackRoutes } from '@/main/routes/mappers/EAppStackRoutes';
-import { useNavigation } from '@react-navigation/native';
 import { AccessibilityInfo } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+
 import Snackbar from 'react-native-snackbar';
 import { theme } from '@/global/theme';
 import { LoadingWithOverlay } from '@/presentation/components/LoadingWithOverlay';
+import { FormSchema, inputs, options } from './data';
+import { IFieldValues } from './props';
+import { Controller, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export const SettingsScreen: React.FC = () => {
 	const { user, logout } = useAuthenticationStore();
 	const [isLoading, setIsLoading] = useState(false);
+	const [open1, setOpen1] = useState(false);
+	const [open2, setOpen2] = useState(false);
+	const [open3, setOpen3] = useState(false);
+  const [items, setItems] = useState(options);
+
+	const {
+    handleSubmit,
+		control,
+		reset,
+    formState: { errors },
+  } = useForm<IFieldValues>({
+		resolver: zodResolver(FormSchema)
+	});
 
 	async function handleLogout() {
 		setIsLoading(true);
@@ -38,6 +55,77 @@ export const SettingsScreen: React.FC = () => {
 	return <Container>
 		<Header title="Configurações" />
 		{isLoading ? <LoadingWithOverlay/>: null}
+		<Controller
+			control={control}
+			name="shape1"
+			render={({ field: { onBlur, onChange, value } }) => {
+				return <DropDownPicker
+					open={open1}
+					value={value}
+					items={items}
+					setOpen={setOpen1}
+					setValue={onChange}
+					setItems={setItems}
+				/>
+			}}
+		/>
+		<Controller
+			control={control}
+			name="shape2"
+			render={({ field: { onBlur, onChange, value } }) => {
+				return <DropDownPicker
+					open={open2}
+					value={value}
+					items={items}
+					setOpen={setOpen2}
+					setValue={onChange}
+					setItems={setItems}
+				/>
+			}}
+		/>
+		<Controller
+			control={control}
+			name="shape3"
+			render={({ field: { onBlur, onChange, value } }) => {
+				return <DropDownPicker
+					open={open3}
+					value={value}
+					items={items}
+					setOpen={setOpen3}
+					setValue={onChange}
+					setItems={setItems}
+				/>
+			}}
+		/>
+			{inputs.map(({name, ...input})=> {
+				const inputName = name as keyof IFieldValues;
+				console.log({inputName})
+				return <Controller
+					control={control}
+					name={inputName}
+					key={inputName}
+					render={({ field: { onBlur, onChange, value } }) => {
+						return (
+							<InputContainer key={inputName}>
+								<Input 
+									{...input}
+									onChangeText={onChange}
+									value={value}
+									onBlur={onBlur}
+								/>
+								{errors[inputName]?.message ?
+									<ErrorText>{errors[inputName]?.message}</ErrorText> 
+									:	null 
+								}
+							</InputContainer>
+						)
+					}}
+				/>
+			})}
+		<OptionButton onPress={handleLogout} color={theme.colors.blue}>	
+			<ButtonText color={theme.colors.white}>Salvar alterações</ButtonText>
+			<Icon name="chevron-right" color={theme.colors.white} />
+		</OptionButton>
 		<OptionButton onPress={handleLogout}>	
 			<ButtonText>Sair</ButtonText>
 			<Icon name="log-out" />
