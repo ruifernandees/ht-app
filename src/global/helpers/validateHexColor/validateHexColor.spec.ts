7c4a628ab285b4392adb3d7b32728e67de57c703
ruifernandees <dev.ruifernandes@gmail.com>
2024-03-20 10:25:55 -0300

    refactor: blank spaces

--- src/global/helpers/validateHexColor/validateHexColor.spec.ts
+++ src/global/helpers/validateHexColor/validateHexColor.spec.ts
@@ -1,34 +1,33 @@
-import { validateHexColor } from ".";
+import {validateHexColor} from '.';
 
 describe('ValidateHexColor function', () => {
+	it('Should check a three-character color', () => {
+		const isValid = validateHexColor('#fff');
+		expect(isValid).toBeTruthy();
+	});
 
-  it('Should check a three-character color', () => {
-    const isValid = validateHexColor('#fff');
-    expect(isValid).toBeTruthy()
-  });
+	it('Should check a six-character color', () => {
+		const isValid = validateHexColor('#ffffff');
+		expect(isValid).toBeTruthy();
+	});
 
-  it('Should check a six-character color', () => {
-    const isValid = validateHexColor('#ffffff');
-    expect(isValid).toBeTruthy()
-  });
+	it('Should check a six-character color with distinct values', () => {
+		const isValid = validateHexColor('#fae2d3');
+		expect(isValid).toBeTruthy();
+	});
 
-  it('Should check a six-character color with distinct values', () => {
-    const isValid = validateHexColor('#fae2d3');
-    expect(isValid).toBeTruthy()
-  });
+	it('Should check a four-character color (invalid)', () => {
+		const isValid = validateHexColor('#f1ee');
+		expect(isValid).toBeFalsy();
+	});
 
-  it('Should check a four-character color (invalid)', () => {
-    const isValid = validateHexColor('#f1ee');
-    expect(isValid).toBeFalsy()
-  });
+	it('Should check an invalid color with digit out of hexadecimal range', () => {
+		const isValid = validateHexColor('#fzz');
+		expect(isValid).toBeFalsy();
+	});
 
-  it('Should check an invalid color with digit out of hexadecimal range', () => {
-    const isValid = validateHexColor('#fzz');
-    expect(isValid).toBeFalsy()
-  });
-
-  it('Should check an invalid color with more than 6 characters', () => {
-    const isValid = validateHexColor('#fffaaab');
-    expect(isValid).toBeFalsy()
-  });
+	it('Should check an invalid color with more than 6 characters', () => {
+		const isValid = validateHexColor('#fffaaab');
+		expect(isValid).toBeFalsy();
+	});
 });
--- src/infra/repositories/adapters/FirebaseUserAdapter/FirebaseUserAdapter.spec.ts
+++ src/infra/repositories/adapters/FirebaseUserAdapter/FirebaseUserAdapter.spec.ts
@@ -1,35 +1,34 @@
-import { FirebaseUserAdapter } from ".";
-import { firebaseMockData } from "./data";
+import {FirebaseUserAdapter} from '.';
+import {firebaseMockData} from './data';
 
 describe('FirebaseUserAdapter', () => {
+	it('Should adapt object from firebase to domain', () => {
+		expect(new FirebaseUserAdapter(firebaseMockData)).toEqual({
+			id: 'RmCwEpg7YMeU4ihCKTTNYcK1zfN2',
+			email: 'john.doe@handtalk.com',
+			name: 'John Doe',
+		});
+	});
 
-  it('Should adapt object from firebase to domain', () => {
-    expect(new FirebaseUserAdapter(firebaseMockData)).toEqual({
-      id: 'RmCwEpg7YMeU4ihCKTTNYcK1zfN2',
-      email: 'john.doe@handtalk.com',
-      name: 'John Doe',
-    })
-  });
+	it('Should throw if email is not provided', () => {
+		const firebaseData = {
+			...firebaseMockData,
+			user: {
+				...firebaseMockData.user,
+				email: null,
+			},
+		};
+		expect(() => new FirebaseUserAdapter(firebaseData)).toThrow();
+	});
 
-  it('Should throw if email is not provided', () => {
-    const firebaseData = {
-      ...firebaseMockData,
-      user: {
-        ...firebaseMockData.user,
-        email: null, 
-      }
-    };
-    expect(() => new FirebaseUserAdapter(firebaseData)).toThrow()
-  });
-
-  it('Should throw if displayName is not provided', () => {
-    const firebaseData = {
-      ...firebaseMockData,
-      user: {
-        ...firebaseMockData.user,
-        displayName: null, 
-      }
-    };
-    expect(() => new FirebaseUserAdapter(firebaseData)).toThrow()
-  });
+	it('Should throw if displayName is not provided', () => {
+		const firebaseData = {
+			...firebaseMockData,
+			user: {
+				...firebaseMockData.user,
+				displayName: null,
+			},
+		};
+		expect(() => new FirebaseUserAdapter(firebaseData)).toThrow();
+	});
 });
